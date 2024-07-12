aa0f4a5692d2d56489d3c65b430d9cb0f4a54b1d
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-07-12 21:41:07 +0100

    folder-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+      <h1 className="text-3xl font-bold text-red-900">Not Hello Hello World!</h1>
     </div>
   );
 }
